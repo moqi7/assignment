53434a5b14829bab6321360a1ba981ad2cc5bfa6
moqi <33152635+moqi7@users.noreply.github.com>
2023-01-13 18:16:23 +0800

    Create 8086addition.asm

---
+++ Principle of Microcomputer/8086addition.asm
@@ -0,0 +1,23 @@
+DATA segment;
+    DATA1 DW 1234H
+    DATA2 DW 5678H
+    SUM DW ?
+    DATA ends
+STACK segment para STACK 'STACK'
+    DW 100 DUP (?)
+    top label word
+    STACK ends
+CODE segment
+    ASSUME CS:CODE,DS:DATA,SS:STACK
+START:MOV AX,DATA
+      MOV DS,AX
+      MOV AX,STACK
+      MOV SS,AX;
+      MOV SP,OFFSET TOP;
+      MOV AX,DATA1
+      ADD AX,DATA2
+      MOV SUM,AX
+      MOV AH,4CH
+      INT 21H;
+      CODE ENDS
+END START
